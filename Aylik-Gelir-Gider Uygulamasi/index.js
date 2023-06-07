e7ba3bf2ff8f710a3ade1dd1882ee7ceda14a9d3
Bunyamin54 <https://github.com/Bunyamin54>
2023-06-07 21:47:07 +0200

    dom

--- Aylik-Gelir-Gider Uygulamasi/index.js
+++ Aylik-Gelir-Gider Uygulamasi/index.js
@@ -12,6 +12,10 @@ const harcamaAlaniInput = document.getElementById("harcama-alani")
 const tarihInput = document.getElementById("tarih")
 const miktarInput = document.getElementById("miktar")
 
+//? Haracama Tablosu
+
+const harcamaBody = document.getElementById("harcama-body")
+
 //? Variables
 let gelirler = 0
 
@@ -40,6 +44,12 @@ window.addEventListener("load", () => {
   //? gelirler bilgisini local storage'dan okuyarak global degiskenimize yaz
   gelirler = Number(localStorage.getItem("gelirler"))
 
+  //? localStroge'den harcama listesini okuyarak global dizimize saklıyoruz.
+  harcamaListesi = JSON.parse(localStorage.getItem("harcamalar")) || []
+
+  harcamaListesi.forEach((h) => console.log(h))
+
+  console.log(harcamaListesi)
   //? Tarih inputunu bugun deger ile yukle
   tarihInput.valueAsDate = new Date()
 
@@ -53,7 +63,7 @@ harcamaFormu.addEventListener("submit", (e) => {
 
   //? yeni harcama bilgileri ile bir obje olusutur
   const yeniHarcama = {
-    id: new Date().getTime(),
+    id: new Date().getTime(), //? Sistem saatini (ms olarak) verir. Unique gibidir.
     tarih: tarihInput.value,
     alan: harcamaAlaniInput.value,
     miktar: miktarInput.value,
@@ -62,13 +72,28 @@ harcamaFormu.addEventListener("submit", (e) => {
   //? yeni harcama objesini diziye ekle
   harcamaListesi.push(yeniHarcama)
 
+  //? dizisin son halini localStorage e gonder.
+  localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi))
+
+  harcamayiDomaYaz(yeniHarcama)
+
   //? Formdaki verileri sil
   harcamaFormu.reset()
   tarihInput.valueAsDate = new Date()
-  console.log(yeniHarcama)
 })
-
 
 const hesaplaVeGuncelle = () => {
   gelirinizTd.innerText = gelirler
+}
+
+const harcamayiDomaYaz = ({ id, miktar, tarih, alan }) => {
+  // const { id, miktar, tarih, alan } = yeniHarcama
+  harcamaBody.innerHTML += `
+  <tr>
+    <td>${tarih}</td>
+    <td>${alan}</td>
+    <td>${miktar}</td>
+    <td><i id=${id} class="fa-solid fa-trash-can text-danger"></i></td>
+  </tr>
+  `
 }
