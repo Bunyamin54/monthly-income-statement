31e78be01bbe39f0be5a81b1a9dd40c80dd465b6
Bunyamin54 <https://github.com/Bunyamin54>
2023-06-07 20:32:56 +0200

    id="ekle-formu"

--- Aylik-Gelir-Gider Uygulamasi/index.js
+++ Aylik-Gelir-Gider Uygulamasi/index.js
@@ -3,17 +3,31 @@ const ekleBtn = document.getElementById("ekle-btn")
 const gelirInput = document.getElementById("gelir-input")
 const ekleFormu = document.getElementById("ekle-formu")
 
+//?
+
+const gelirinizTd= document.getElementById("geliriniz")
+
+
 //? Variables
-let gelirler = ""
+let gelirler = 0
 
 //?Events
 
 //? Formun submit butonuna basildiginda
-ekleFormu.addEventListener("submit", (e) => {
+ ekleFormu.addEventListener("submit", (e) => {
   e.preventDefault() //? reload'u engeller
-  gelirler += gelirInput.value
+  gelirler  = gelirler + Number(gelirInput.value) //? string eklemeyi engelledik
 
-  console.log(gelirler)
+// ? input degerini sifirladik 
+  ekleFormu.reset()
+  
+
+  //? Degisikleri doma basan fonksyion
+hesaplaVeGuncelle()
 })
 
 //? Functions
+const hesaplaVeGuncelle = () => {
+
+gelirinizTd.innerText = gelirler
+}
