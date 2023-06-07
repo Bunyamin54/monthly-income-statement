ef5476896c5a4c4cabef051bfb58707875784e66
Bunyamin54 <https://github.com/Bunyamin54>
2023-06-07 21:41:28 +0200

    selcetors

--- Aylik-Gelir-Gider Uygulamasi/index.js
+++ Aylik-Gelir-Gider Uygulamasi/index.js
@@ -3,42 +3,72 @@ const ekleBtn = document.getElementById("ekle-btn")
 const gelirInput = document.getElementById("gelir-input")
 const ekleFormu = document.getElementById("ekle-formu")
 
-//?
+//? Sonuc tablosu
+const gelirinizTd = document.getElementById("geliriniz")
 
-const gelirinizTd= document.getElementById("geliriniz")
-
+//? harcama formu
+const harcamaFormu = document.getElementById("harcama-formu")
+const harcamaAlaniInput = document.getElementById("harcama-alani")
+const tarihInput = document.getElementById("tarih")
+const miktarInput = document.getElementById("miktar")
 
 //? Variables
 let gelirler = 0
 
+//? tum harcamalari saklayacak dizi (JSON)
+let harcamaListesi = []
+
 //?Events
 
-//? Formun submit butonuna basildiginda
- ekleFormu.addEventListener("submit", (e) => {
+//! Formun submit butonuna basildiginda
+ekleFormu.addEventListener("submit", (e) => {
   e.preventDefault() //? reload'u engeller
-  gelirler  = gelirler + Number(gelirInput.value) //? string eklemeyi engelledik
+  gelirler = gelirler + Number(gelirInput.value) //? string eklemiyi engelledik
 
+  //? gelirlerin kalıcı olmasi icin localStorage a kopyaliyoruz
+  localStorage.setItem("gelirler", gelirler)
 
-localStorage.setItem("gelirler", gelirler)
-
-
-// ? input degerini sifirladik 
+  //? input degerini sifrladik
   ekleFormu.reset()
-  
 
-  //? Degisikleri doma basan fonksyion
-hesaplaVeGuncelle()
+  //? Degisiklikleri sonuc tablosuna yazan fonks.
+  hesaplaVeGuncelle()
 })
 
-//! sayfa her yuklendikten sonra calisan event 
+//! Sayfa her yuklendikten sonra calisan event
 window.addEventListener("load", () => {
+  //? gelirler bilgisini local storage'dan okuyarak global degiskenimize yaz
+  gelirler = Number(localStorage.getItem("gelirler"))
 
-gelirler =Number (localStorage.getItem("gelirler"))
-hesaplaVeGuncelle()
+  //? Tarih inputunu bugun deger ile yukle
+  tarihInput.valueAsDate = new Date()
 
+  //? Degisen bilgileri hesapla ve DOM'a bas
+  hesaplaVeGuncelle()
 })
-//? Functions
-const hesaplaVeGuncelle = () => {
 
-gelirinizTd.innerText = gelirler
+//! harcama formu submit edildiginde calisir
+harcamaFormu.addEventListener("submit", (e) => {
+  e.preventDefault() //? reload'u engelle
+
+  //? yeni harcama bilgileri ile bir obje olusutur
+  const yeniHarcama = {
+    id: new Date().getTime(),
+    tarih: tarihInput.value,
+    alan: harcamaAlaniInput.value,
+    miktar: miktarInput.value,
+  }
+
+  //? yeni harcama objesini diziye ekle
+  harcamaListesi.push(yeniHarcama)
+
+  //? Formdaki verileri sil
+  harcamaFormu.reset()
+  tarihInput.valueAsDate = new Date()
+  console.log(yeniHarcama)
+})
+
+
+const hesaplaVeGuncelle = () => {
+  gelirinizTd.innerText = gelirler
 }
