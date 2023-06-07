502e889523434bdacfdffd46c6ebcc0bcffd9d85
Bunyamin54 <https://github.com/Bunyamin54>
2023-06-07 14:51:11 +0200

    value

--- monthly-income-statement/index.js
+++ monthly-income-statement/index.js
@@ -7,9 +7,26 @@ let dateTime = document.getElementById("date-time")
 let amount = document.getElementById("amount")
 let saveButton = document.getElementById("save")
 
+
+let jSonArr = []
+
 // ****** Eventlistener *******
 
 inputAddButton.addEventListener("click",() => {
 
    console.log(inputIncome.value);
 }) 
+
+saveButton.addEventListener("click", () => {
+ 
+   let jSonObj = {
+
+      dt : dateTime.value,
+      exp:expensesType.value,
+      amnt: amount.value
+   }
+
+   jSonObj.push(jSonArr)
+   console.log(jSonObj);
+})
+
